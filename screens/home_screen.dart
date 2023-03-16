c44745afe3d10e6f02d78ec63ad23a1b3f1becf0
Byeol2codingtest <69844005+Byeol2codingtest@users.noreply.github.com>
2023-03-16 23:27:21 +0900

    Add files via upload

---
+++ screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        title: const Text("Today's 툰s"),
+      ),
+    );
+  }
+}
